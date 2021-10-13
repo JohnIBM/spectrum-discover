0fabac05a4fb7ebd2864636e301246fd20807fa6
JohnIBM <43707629+JohnIBM@users.noreply.github.com>
2021-10-13 11:10:15 -0400

    Tiller updates

--- tiller-installation/tiller_install.sh
+++ tiller-installation/tiller_install.sh
@@ -4,10 +4,11 @@ export TILLER_NAMESPACE=tiller
 oc new-project $TILLER_NAMESPACE
 curl -s https://get.helm.sh/helm-v2.17.0-linux-amd64.tar.gz|tar xz 
 cd linux-amd64/
+# Client install
 ./helm init --client-only
 cp ./helm /usr/bin/
 
-
+# Server Install
 wget -q https://github.com/openshift/origin/raw/master/examples/helm/tiller-template.yaml
 # The following step corrects the image location for Helm v2
 sed -i 's|gcr.io\/kubernetes-helm\/tiller|ghcr.io\/helm\/tiller|g' tiller-template.yaml
@@ -15,6 +16,6 @@ oc process -f tiller-template.yaml -p TILLER_NAMESPACE="${TILLER_NAMESPACE}" -p 
 
 
 helm version 
-You should see:
-Client: &version.Version{SemVer:"v2.17.0", GitCommit:"a690bad98af45b015bd3da1a41f6218b1a451dbe", GitTreeState:"clean"}
-Server: &version.Version{SemVer:"v2.17.0", GitCommit:"a690bad98af45b015bd3da1a41f6218b1a451dbe", GitTreeState:"clean"}
+# You should see:
+# Client: &version.Version{SemVer:"v2.17.0", GitCommit:"a690bad98af45b015bd3da1a41f6218b1a451dbe", GitTreeState:"clean"}
+# Server: &version.Version{SemVer:"v2.17.0", GitCommit:"a690bad98af45b015bd3da1a41f6218b1a451dbe", GitTreeState:"clean"}
